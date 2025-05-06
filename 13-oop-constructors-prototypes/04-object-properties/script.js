521142c8b3a44c54a70ee374fc12b2962c0904cf
Jay Ryan Nacua <jayryannacua11@gmail.com>
2025-05-07 05:55:35 +0800

    OOP init

--- 13-oop-constructors-prototypes/04-object-properties/script.js
+++ 13-oop-constructors-prototypes/04-object-properties/script.js
@@ -6,3 +6,39 @@ function Rectangle(name, width, height) {
     return this.width * this.height;
   };
 }
+
+const rect1 = new Rectangle('Rectangle 1', 20, 20);
+const rect2 = new Rectangle('Rectangle 2', 30, 30);
+
+console.log(rect1.name, rect2.width);
+
+rect1.color = 'red';
+console.log(rect1);
+
+rect2.perimeter = () => 2 * (rect2.width + rect2.height);
+
+console.log(rect2.perimeter());
+
+// Remove property
+delete rect2.perimeter;
+
+// Check for property
+console.log(rect2.hasOwnProperty('color'));
+console.log(rect1.hasOwnProperty('color'));
+
+// Get keys
+console.log(Object.keys(rect1));
+
+// Get values
+console.log(Object.values(rect2));
+
+// Get entries
+console.log(Object.entries(rect1));
+
+for(let [key, value] of Object.entries(rect1)){
+  if (typeof value !== 'function') {
+    console.log(`${key} - ${value}`);
+  }
+}
+
+console.log(rect2);
