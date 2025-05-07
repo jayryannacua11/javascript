80738f19047b1a5035c61ee7d2d3215b9724fb6c
Jay Ryan Nacua <jayryannacua11@gmail.com>
2025-05-07 21:51:49 +0800

    Prototypes

--- 13-oop-constructors-prototypes/06-add-prototype-methods/script.js
+++ 13-oop-constructors-prototypes/06-add-prototype-methods/script.js
@@ -2,12 +2,30 @@ function Rectangle(name, width, height) {
   this.name = name;
   this.width = width;
   this.height = height;
-  this.area = function () {
-  	return this.width * this.height;
-  };
 }
 
+Rectangle.prototype.area = function () {
+  return this.width * this.height;
+}
+
+Rectangle.prototype.perimeter = function () {
+  return 2 * (this.width + this.height);
+}
+
+Rectangle.prototype.isSquare = function () {
+  return this.width === this.height;
+}
+
+Rectangle.prototype.changeName = function (newName) {
+  return this.name = newName;
+}
 
 const rect = new Rectangle('Rect', 10, 10);
+const rect2 = new Rectangle('Rect 2', 30, 40);
 
 console.log(rect);
+console.log(rect.area());
+console.log(rect.perimeter());
+console.log(rect.isSquare());
+console.log(rect.changeName('Test'));
+console.log(rect.name);
